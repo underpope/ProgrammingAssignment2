a05fee742a47bd25777e3b82957b75b7800b84f4
Richard Crawford <richard@underpope.com>
2016-12-25 12:07:35 -0800

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+# Create the matrix in another workspace. It feels a bit like storing
+# it in "subspace" because I'm a Star Trek fan.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+    subspace <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    
+    get <- function() x
+    
+    setinverse <- function(inverse) inv <<- inverse
+    
+    getinverse <- function() inv
+    
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)	
 }
 
-
-## Write a short comment describing this function
-
+# Return the inverse of the matrix.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    inv <- x$getinverse()
+    
+    if(!is.null(inv)) {
+        return(inv)
+    }
+    
+    data <- x$get()
+    
+    inv <- solve(data, ...)
+    
+    x$setinverse(inv)
+    
+    inv    
 }
